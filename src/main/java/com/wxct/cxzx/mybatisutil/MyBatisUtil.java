a8bd2f6541993b083daf6b00613233045538f570
zhenr <zhenr02nju@gmail.com>
2018-12-05 15:28:15 +0800

    加入environment参数 多数据源选择

--- src/main/java/com/wxct/cxzx/mybatisutil/MyBatisUtil.java
+++ src/main/java/com/wxct/cxzx/mybatisutil/MyBatisUtil.java
@@ -33,10 +33,29 @@ public class MyBatisUtil {
 	}
 	
 	/**
+	 * 获取SqlSessionFactory
+	 * @param environment 多数据源时 选择数据源
+	 * */
+	public SqlSessionFactory getSqlSessionFactory(String environment) {
+		InputStream is = MyBatisUtil.class.getClassLoader().getResourceAsStream(resource);
+		SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(is,environment);
+		return factory;
+	}
+	
+	/**
 	 * 获取SqlSession
 	 * */
 	public SqlSession getSqlSession() {
 		return getSqlSessionFactory().openSession();
+	}
+	
+	/**
+	 * 获取SqlSession
+	 * 
+	 * @param environment 多数据源时 选择数据源
+	 * */
+	public SqlSession getSqlSession(String environment) {
+		return getSqlSessionFactory(environment).openSession();
 	}
 	
 	/**
@@ -47,5 +66,16 @@ public class MyBatisUtil {
 	 * */
 	public SqlSession getSqlSession(boolean isAutoCommit) {
 		return getSqlSessionFactory().openSession(isAutoCommit);
+	}
+	
+	/**
+	 * 获取SqlSession
+	 * @param isAutoCommit 
+     *        true 表示创建的SqlSession对象在执行完SQL之后会自动提交事务
+     *        false 表示创建的SqlSession对象在执行完SQL之后不会自动提交事务，这时就需要我们手动调用sqlSession.commit()提交事务
+     * @param environment 多数据源时 选择数据源
+	 * */
+	public SqlSession getSqlSession(String environment,boolean isAutoCommit) {
+		return getSqlSessionFactory(environment).openSession(isAutoCommit);
 	}
 }
